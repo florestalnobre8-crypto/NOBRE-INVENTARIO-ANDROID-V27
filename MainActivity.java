b1242e08f1bf8a6a748bedd837d6ceb8f6bd4023
florestalnobre8-crypto <florestalnobre8@gmail.com>
2026-09-20 20:28:01 -0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -212,7 +212,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        webView.loadUrl("file:///android_asset/index.html?android=1&build=46");
+        webView.loadUrl("file:///android_asset/index.html?android=1&build=47");
     }
 
     public class GpsBridge {
